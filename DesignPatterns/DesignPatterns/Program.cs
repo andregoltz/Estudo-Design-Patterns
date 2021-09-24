dabc3db0c3301ad6e3a5b7fb3f0bd23ee1e930a9
André Goltz <andregoltz@outlook.com.br>
2021-09-24 08:25:43 -0300

    Estudando sobre Singleton

--- DesignPatterns/DesignPatterns/Program.cs
+++ DesignPatterns/DesignPatterns/Program.cs
@@ -10,6 +10,16 @@ namespace DesignPatterns
     {
         static void Main(string[] args)
         {
+            SingletonPattern jogador_1 = SingletonPattern.GetInstancia;
+            jogador_1.Mensagem("Jogador 1: A bola está comigo no meio do campo.");
+
+            SingletonPattern jogador_2 = SingletonPattern.GetInstancia;
+            jogador_2.Mensagem("Jogador 2: recebeu a bola.");
+
+            SingletonPattern jogador_3 = SingletonPattern.GetInstancia;
+            jogador_3.Mensagem("Jogador 3: recebeu o lançamento na linha de fundo.");
+
+            Console.ReadKey();
         }
     }
 }
---
+++ DesignPatterns/DesignPatterns/SingletonPattern.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns
+{
+    public sealed class SingletonPattern
+    {
+        private static SingletonPattern instancia = null;
+
+        public static SingletonPattern GetInstancia
+        { 
+            get
+            {
+                if (instancia == null)
+                {
+                    instancia = new SingletonPattern();
+                    Console.WriteLine("Bola em Jogo");
+                }
+                return instancia;
+            } 
+        }
+
+        public void Mensagem(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+    }
+}
