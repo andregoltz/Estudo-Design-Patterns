7ef2854fb51d8b493b80276d2e07b9bd7a322880
André Goltz <andregoltz@outlook.com.br>
2021-09-24 10:16:49 -0300

    Estudando sobre Prototype

---
+++ DesignPatterns/Prototype/NuvemConcreta.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype
+{
+    public class NuvemConcreta : NuvemMolde
+    {
+        private string cor_preenchimento;
+        private string cor_contorno;
+        public NuvemConcreta(string prenchimento, string contorno)
+        {
+            this.cor_preenchimento = prenchimento;
+            this.cor_contorno = contorno;
+        }
+        public override NuvemMolde Clone()
+        {
+            Console.WriteLine("A nuvem clonada tem contorno " + this.cor_contorno + " e preenchimento " + this.cor_preenchimento);
+            return this.MemberwiseClone() as NuvemMolde;
+        }
+    }
+}
